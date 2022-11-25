4ed7474c96f4f20974f0cd8f9e5da4fb66859bc7
Artem <artem20072003@outlook.com>
2022-11-25 08:38:48 +0300

    Create contract.sol

---
+++ contract.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "hardhat/console.sol";
+contract artem {
+
+    struct massagist { 
+    string name;
+    string age;
+    string[] customers;
+    uint profit;
+}
+    massagist[] private worker;
+
+    mapping(string => uint) prices;
+    string[] servives;
+
+    function new_worker(string memory name, string memory age) public payable {
+       require(bytes(name).length > 0, "Name cannot be left empty!");
+       require(bytes(name).length > 0, "Age cannot be left empty!");
+        bool exist = true;
+        for (uint i = 0; i<worker.length; i++)
+        {
+            if ((keccak256(abi.encodePacked(name))) == (keccak256(abi.encodePacked(worker[i].name))))
+            {
+                exist=false;
+            }
+        }
+        require(exist, "already in the system!");
+        
+        uint256 i2 = worker.length;
+        worker.push();
+        worker[i2].name = name;
+        worker[i2].age = age;
+        worker[i2].profit = 0;
+    }
+
+    function make_appointment(string memory massagist_name, string memory my_name) public payable {
+        require(bytes(massagist_name).length > 0, "massagist name cannot be left empty!");
+        require(bytes(my_name).length > 0, "Your name cannot be left empty!");
+       
+        
+        bool flag = false;
+        for (uint i = 0; i<worker.length; i++)
+        {
+            if ((keccak256(abi.encodePacked(worker[i].name))) == (keccak256(abi.encodePacked(massagist_name))))
+            {
+                worker[i].customers.push(my_name);
+                flag = true;
+            }
+        }
+        require(flag, "We don't know this massagist!");
+    }
+
+
+    function new_service(string memory services_name, uint price) public payable {
+        require(bytes(services_name).length > 0, "service's name cannot be left empty!");
+        prices[services_name] = price;
+        servives.push(services_name);
+    }
+
+    function all_massagists() public payable {
+        for (uint i = 0; i<worker.length; i++)
+        {
+            console.log("Massagist ", worker[i].name, " at the age of ", worker[i].age);
+        }      
+    }
+
+    function pricelist() public payable {
+        for (uint i = 0; i<servives.length; i++)
+        {
+            console.log("Servive ", servives[i], " at the price of ", prices[servives[i]]);
+        }      
+    }
+
+    function massagists_customers(string memory name) public payable {
+        require(bytes(name).length > 0, "name cannot be left empty!");
+        for (uint i = 0; i<worker.length; i++)
+        {
+            if ((keccak256(abi.encodePacked(worker[i].name))) == (keccak256(abi.encodePacked(name))))
+            {
+                console.log("Customers of ", worker[i].name);
+                for (uint j = 0; j<worker[i].customers.length; j++)
+                {
+                        console.log("Customer ", worker[i].customers[j]);
+                }
+            }
+        }      
+    }
+
+
+}
